7b4c8314884cdbc2fe4b125aaf5081a96118fbbc
LaZY <18846171630@163.com>
2019-02-23 13:55:21 +0800

    增加：hadoop-Spark：验证持久化--代码

---
+++ SparkDemo/src/main/java/Persist.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @author LaZY(李志一) 
+  * @create 2019-02-23 13:50 
+  */
+object Persist {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setAppName("Persist")
+    conf.setMaster("local[4,3]")//可重试3次
+    val sc = new SparkContext(conf)
+
+    val rdd1 = sc.parallelize(1 to 20);
+    val rdd2 = rdd1.map(e=>{
+      println(e)
+      e * 2
+    })
+    rdd2.persist(StorageLevel.DISK_ONLY)
+    println(rdd2.reduce(_+_))
+    println(rdd2.reduce(_+_))
+  }
+
+}
